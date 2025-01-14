46f78dcbf67558630c55b1475d52072d0a008612
Rxbsi <robinfranz02@gmail.com>
2025-01-14 22:14:16 +0100

    Pushing the project base

---
+++ src/main/java/de/rxbsi/stempelsystem/StempelsystemApplication.java
@@ -0,0 +1,13 @@
+package de.rxbsi.stempelsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StempelsystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StempelsystemApplication.class, args);
+    }
+
+}
